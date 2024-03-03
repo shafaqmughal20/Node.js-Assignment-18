481c378135197cb55e1b72a9eacc4395c4ea7109
shafaqmughal20 <160257641+shafaqmughal20@users.noreply.github.com>
2024-03-03 07:46:19 +0500

    Add files via upload

---
+++ Q-18.ts
@@ -0,0 +1,39 @@
+//18. Seeing the World: Think of at least five places in the world you’d like to visit.
+//• Store the locations in a array. Make sure the array is not in alphabetical order.
+
+//• Print your array in its original order.
+
+//• Print your array in alphabetical order without modifying the actual list.
+
+//• Show that your array is still in its original order by printing it.
+
+//• Print your array in reverse alphabetical order without changing the order of the original list.
+
+//• Show that your array is still in its original order by printing it again.
+
+//• Reverse the order of your list. Print the array to show that its order has changed.
+
+//• Reverse the order of your list again. Print the list to show it’s back to its original order.
+
+//• Sort your array so it’s stored in alphabetical order. Print the array to show that its order has been changed.
+
+//• Sort to change your array so it’s stored in reverse alphabetical order. Print the list to show that its order has changed.
+
+let countriesToVisit:string []=["Saudia", "China" ,"Enland","Australia","UAE"]
+//console.log("Original Order:", countriesToVisit);
+
+//console.log("Alphabetical Order:" ,[...countriesToVisit].sort())
+
+//console.log("Still in Original Order:", countriesToVisit)
+
+//console.log("Reverse order:", [...countriesToVisit].reverse())
+
+//console.log("Still in Original Order:", countriesToVisit)
+
+//console.log("Original Array Reverse:", countriesToVisit.reverse())
+
+//console.log("Back to Original Order:", countriesToVisit.reverse())
+
+//console.log("Sorted in Alphabetcal Order:",countriesToVisit)
+
+//console.log("Original Array Reverse Again:", countriesToVisit.reverse())
